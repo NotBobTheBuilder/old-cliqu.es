5960ccb832ddb7420c07b039d57a7360a5b1cc1f
Jack Wearden <jack@jackwearden.co.uk>
2014-01-19 12:04:14 +0000

    Initial iCal template

--- views/index.js
+++ views/index.js
@@ -2,6 +2,7 @@ var mustachex = require("mustachex");
 
 module.exports = function(app) {
   app.engine("mst", mustachex.express);
+  app.engine("ics", mustachex.express);
   app.set("view engine", "mst");
   app.set("views", "./views/mustache");
 };

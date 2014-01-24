17078c503be440bede1790fb343d31e4af6318b8
Jack Wearden <jack@jackwearden.co.uk>
2014-01-24 11:35:09 +0000

    Custom settings and defaults are held separately
    this closes #20

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
 var settings  = require("./settings.json"),
+    defaults  = require("./defaults.json"),
+
+    _         = require("underscore"),
     mode      = process.env.NODE_MODE || "development";
 
 
-module.exports = settings[mode];
+module.exports = _.defaults(settings[mode] || {}, defaults[mode]);

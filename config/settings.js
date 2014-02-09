30ddf7d2fdc12a1662ce455b092dec3708e20618
Jack Wearden <jack@jackwearden.co.uk>
2014-02-09 16:53:29 +0000

    Settings.js checks environment
    so far for port and twitter keys
    
    Fixing #55

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,14 @@
-module.exports = {
-  "development": {
-    "twitter": false
-  }
-};
+var settings = {};
+
+if (process.env.twitter) {
+  settings.twitter = {
+    "key": process.env.twitter.split(":")[0],
+    "secret": process.env.twitter.split(":")[1],
+  };
+  console.log(settings);
+}
+
+if (process.env.NODE_PORT)
+  settings.port = parseInt(process.env.NODE_PORT, 10);
+
+module.exports = settings;
